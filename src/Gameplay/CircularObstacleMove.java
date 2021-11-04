e9dc4562fbef33a1d3968492d022244e07f41843
EMIRHAN EREL <eerel18@ku.edu.tr>
2021-11-05 02:03:29 +0300

    Upload New File

---
+++ src/Gameplay/CircularObstacleMove.java
@@ -0,0 +1,11 @@
+package Gameplay;
+
+import Body.Obstacle.Obstacle;
+
+public class CircularObstacleMove implements ObstacleMove{
+
+    @Override
+    public void move(Obstacle obstacle, double probability) {
+        
+    }
+}
