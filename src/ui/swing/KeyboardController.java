833764cde0cab88b262dcbcba95c917b6e0e1643
kgirenes18 <kgirenes18@ku.edu.tr>
2021-12-02 00:29:21 +0300

    KeyboardController created

---
+++ src/ui/swing/KeyboardController.java
@@ -0,0 +1,22 @@
+package ui.swing;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+public class KeyboardController implements KeyListener {
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Reserved for future use
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Reserved for future use
+    }
+}
