b0391b2bbaa89db8036e58236b9e0e87c1b37d46
kgirenes18 <kgirenes18@ku.edu.tr>
2021-12-04 22:37:22 +0300

    emirhan - loadAndSave implementation further

---
+++ src/domain/loadAndSave/DatabaseLoadAndSave.java
@@ -0,0 +1,16 @@
+package domain.loadAndSave;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DatabaseLoadAndSave implements ILoadAndSaveAdapter{
+    @Override
+    public void saveGame(ArrayList<String> saveList, String username) {
+
+    }
+
+    @Override
+    public ArrayList<String> loadGame(String username) {
+        return null;
+    }
+}
