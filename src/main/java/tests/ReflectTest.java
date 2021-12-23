3c82280ef8fa31391a6d68ace0ab3399605fe10f
EGE BERK YILDIRIM <eyildirim18@ku.edu.tr>
2021-12-23 15:23:39 +0300

    Added comments to ReflectTest.

--- src/main/java/tests/ReflectTest.java
+++ src/main/java/tests/ReflectTest.java
@@ -10,6 +10,7 @@ import domain.body.EnchantedSphere;
 public class ReflectTest {
 
     @Test
+    //Testing if es reflects from left wall correctly.
     public void reflectFromLeftWall(){
         EnchantedSphere es = new EnchantedSphere(0,200,12,12,
                 new NoblePhantasm(450,570,100,8));
@@ -20,6 +21,7 @@ public class ReflectTest {
     }
 
     @Test
+    //Testing if es reflects from upper wall correctly
     public void reflectFromUpperWall(){
         EnchantedSphere es = new EnchantedSphere(100,0,12,12,
                 new NoblePhantasm(450,570,100,8));
@@ -30,6 +32,7 @@ public class ReflectTest {
     }
 
     @Test
+    //Testing if es reflects from rotated NP (-45 degrees specifically) correctly.
     public void reflectFromRotatedNP(){
         NoblePhantasm np = new NoblePhantasm(450,570,100,8);
         EnchantedSphere es = new EnchantedSphere(494,558,12,12, np);
@@ -44,6 +47,7 @@ public class ReflectTest {
     }
 
     @Test
+    //Testing if es reflects from the corner of game screen correctly.
     public void reflectFromCorner(){
         EnchantedSphere es = new EnchantedSphere(0,0,12,12,
                 new NoblePhantasm(450,570,100,8));
@@ -56,6 +60,7 @@ public class ReflectTest {
     }
 
     @Test
+    //Testing if es reflects from an obstacle correctly.
     public void reflectFromObstacle(){
         EnchantedSphere es = new EnchantedSphere(100,92,12,12,
                 new NoblePhantasm(450,570,100,8));
