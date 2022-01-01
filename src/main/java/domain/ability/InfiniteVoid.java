0ba14d8361e50d40767d2d276556c4c42bf3c0e9
Büşra Işık <bisik18@ku.edu.tr>
2022-01-01 21:50:14 +0300

    Infinite Void is implemented.

--- src/main/java/domain/ability/InfiniteVoid.java
+++ src/main/java/domain/ability/InfiniteVoid.java
@@ -1,9 +1,29 @@
 package domain.ability;
+import domain.body.obstacle.Obstacle;
+import domain.needForSpear.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class InfiniteVoid extends Ability {
-
+    ArrayList<Obstacle> obstacles = Statistics.getObstacleList();
     public InfiniteVoid() {
         super();
         name = "Infinite Void";
+        freeze();
+    }
+
+    public void freeze(){
+        ArrayList<Obstacle> obstaclesLeft = new ArrayList<>();
+        for(Obstacle obstacle : obstacles){
+            if(!obstacle.getName().equals("Hollow")){
+                obstaclesLeft.add(obstacle);
+            }
+        }
+        //Shuffles the list so the first 8 chosen are random.
+        Collections.shuffle(obstaclesLeft);
+        for(int i=0; i<Math.min(8,obstaclesLeft.size()); i++){
+            obstaclesLeft.get(i).setFrozen(true);
+        }
     }
 }
