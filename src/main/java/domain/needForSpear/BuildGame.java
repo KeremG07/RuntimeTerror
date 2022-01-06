651b5423b0b07df37d5f00a3872f944541bf93a6
Umur Berkay Karakas <ukarakas18@ku.edu.tr>
2022-01-06 11:42:54 +0100

    BuildGame new implementation

--- src/main/java/domain/needForSpear/BuildGame.java
+++ src/main/java/domain/needForSpear/BuildGame.java
@@ -13,33 +13,56 @@ public class BuildGame {
     public final double gameScreenWidth = Controller.getInstance().getFrameBorders()[0],
             gameScreenHeight = Controller.getInstance().getFrameBorders()[1];
     private Random randi = new Random();
-
+    private Statistics statistics;
     //Creates however many obstacles were asked to be created.
     public BuildGame(String[] numOfObstaclesReq){
         //REQUIRES: A string array of length 4.
         //MODIFIES: obstacleList, an ArrayList that contains all the obstacles created in Statistics.java.
         //EFFECTS: Initializes simpleObstacle, firmObstacle, explosiveObstacle and giftObstacle according to user input,
         //creates correct number of obstacles and puts them on the screen randomly and in a way that they do not collide.
+        statistics = Controller.getInstance().getStatistics();
         setNumObstacles(numOfObstaclesReq);
         //The GameScreen is divided into cells (size: 10x10) where objects can be put.
         boolean[][] locationCells = new boolean[(int)(gameScreenHeight-200)/40][(int) gameScreenWidth/100];
         for(int i=0; i<simpleObstacle; i++){
-            putObstacleInCell(locationCells, "Simple");
+            addObstacle("Simple", 1);
         }
         for(int i=0; i<firmObstacle; i++){
-            putObstacleInCell(locationCells, "Firm");
-        }
-        for(int i=0; i<explosiveObstacle; i++){
-            putObstacleInCell(locationCells, "Explosive");
+            int hitNum = randi.nextInt(3) + 3;
+            addObstacle("Firm", hitNum);
         }
         for(int i=0; i<giftObstacle; i++){
-            putObstacleInCell(locationCells, "Gift");
+            addObstacle("Gift", 1);
+        }
+        for(int i=0; i<explosiveObstacle; i++){
+            addObstacle("Explosive", 1);
         }
     }
 
+    public void addObstacle(String type, int hitCount) {
+        boolean notOverlaps = true;
+        int x;
+        int y;
+        while (notOverlaps) {
+            x = randi.nextInt(900);
+            y = randi.nextInt(392);
+            for (Obstacle o: statistics.getObstacleList()) {
+                if (o.compareCoordinates(x,y, 100, 8)) {
+                    notOverlaps = false;
+                    break;
+                }
+            }
+            if (notOverlaps) {
+                Controller.getInstance().getStatistics().addObstacle(BodyFactory.createObstacle(type, x,y,hitCount));
+                return;
+            } else {
+                addObstacle(type, hitCount);
+            }
+        }
+    }
 
     //Finding an empty cell and creating the obstacle there.
-    public void putObstacleInCell(boolean [][]locationCells, String typeOfObstacle){
+    /*public void putObstacleInCell(boolean [][]locationCells, String typeOfObstacle){
         int column;
         int row;
         column = randi.nextInt(10);
@@ -62,7 +85,7 @@ public class BuildGame {
         }
         Controller.getInstance().getStatistics().addObstacle(newCreatedObstacle);
         locationCells[row][column] = true;
-    }
+    }*/
 
     //Gets the number of obstacles entered as input from the user.
     public void setNumObstacles(String[] numOfObstaclesReq) {
@@ -90,13 +113,13 @@ public class BuildGame {
         catch(NumberFormatException exception) {
             giftObstacle = giftObstacleReq;
         }
-        if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 100){
+        if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 200){
             giftObstacle = giftObstacleReq;
-            if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 100) {
+            if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 200) {
                 explosiveObstacle = explosiveObstacleReq;
-                if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 100){
+                if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 200){
                     firmObstacle = firmObstacleReq;
-                    if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 100){
+                    if(simpleObstacle + firmObstacle + explosiveObstacle + giftObstacle >= 200){
                         simpleObstacle = simpleObstacleReq;
                     }
                 }
